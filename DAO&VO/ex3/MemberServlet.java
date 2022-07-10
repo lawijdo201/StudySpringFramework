f57b6f905268b055bb30fd2d4d04b7f885faea8c
SeHyun Bang <86397164+lawijdo201@users.noreply.github.com>
2022-07-10 18:02:53 +0900

    Update MemberServlet.java

--- DAO&VO/ex3/MemberServlet.java
+++ DAO&VO/ex3/MemberServlet.java
@@ -80,26 +80,3 @@ public class MemberServlet extends HttpServlet {
 		out.print("<a href='/pro07/memberForm.html'>새 회원 등록하기</a>");
 	}
 }
-
-/*
-request.setCharacterEncoding("utf-8");
-response.setContentType("text/html;charset=utf-8");
-PrintWriter out = response.getWriter();
-MemberDAO dao = new MemberDAO();
-List<MemberVO>	list = dao.listMembers();
-
-out.print("<html><body>");
-out.print("<table  border=1><tr align='center' bgcolor='lightgreen'>");
-out.print("<td>아이디</td><td>비밀번호</td><td>이름</td><td>이메일</td><td>가입일</td></tr>");
-
-for (int i=0;i<list.size();i++) {
-	MemberVO memberVO = list.get(i);	//새로운 VO를 만들어서 한 튜플씩 저장되어있는 list<Member>로부터 데이터를 읽어옴.
-	String id = memberVO.getId();
-	String pwd = memberVO.getPwd();
-	String name=memberVO.getName();
-	String email=memberVO.getEmail();
-	Date joinDate = memberVO.getJoinDate();
-	out.print("<tr><td>"+id+"</td><td>"+pwd+"</td><td>"+name+"</td><td>"+email+"</td><td>"+joinDate+"</td></tr>");	
-}
-out.print("</table></body></html>");
-*/
