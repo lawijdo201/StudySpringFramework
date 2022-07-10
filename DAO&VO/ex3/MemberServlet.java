a08c570d51de742a0d94910be9686c2df7cf7e61
SeHyun Bang <86397164+lawijdo201@users.noreply.github.com>
2022-07-10 18:02:37 +0900

    Create MemberServlet.java

---
+++ DAO&VO/ex3/MemberServlet.java
@@ -0,0 +1,105 @@
+package sec02.ex02;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Date;
+import java.util.List;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/member2")
+public class MemberServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doHandle(request, response);
+	}
+	
+	private void doHandle(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		MemberDAO dao = new MemberDAO();
+		String command = request.getParameter("command");
+		/*addMember()*/
+		if(command != null&&command.equals("addMember")) {
+			String _id = request.getParameter("id");
+			String _pwd = request.getParameter("pwd");
+			String _name = request.getParameter("name");
+			String _email = request.getParameter("email");
+			MemberVO vo = new MemberVO();
+			vo.setId(_id);
+			vo.setPwd(_pwd);
+			vo.setName(_name);
+			vo.setEmail(_email);
+			dao.addMember(vo);
+			
+		}/*delMember()*/
+		else if(command != null&&command.equals("delMember")) {
+			String id = request.getParameter("id");
+			dao.delMember(id);
+		}
+		List<MemberVO> list = dao.listMembers();	//listMember: select 실행 후 vo에 저장 후 list<MemberVO> 리턴
+		out.print("<html><body>");
+		out.print("<table  border=1><tr align='center' bgcolor='lightgreen'>");
+		out.print("<td>아이디</td><td>비밀번호</td><td>이름</td><td>이메일</td><td>가입일</td><td>삭제</td></tr>");
+
+		for (int i=0;i<list.size();i++) {
+			MemberVO memberVO = list.get(i);	//새로운 VO를 만들어서 한 튜플씩 저장되어있는 list<Member>로부터 데이터를 읽어옴.
+			String id = memberVO.getId();
+			String pwd = memberVO.getPwd();
+			String name=memberVO.getName();
+			String email=memberVO.getEmail();
+			Date joinDate = memberVO.getJoinDate();
+			out.print("<tr><td>"+id+"</td><td>"+pwd+"</td><td>"+name+"</td><td>"+email+"</td><td>"+joinDate
+					+"</td><td>"+"<a href='/pro07/member2?command=delMember&id="+id+"'>삭제</a></td></tr>");	
+		}
+		out.print("</table></body></html>");
+		out.print("<a href='/pro07/memberForm.html'>새 회원 등록하기</a>");
+	}
+}
+
+/*
+request.setCharacterEncoding("utf-8");
+response.setContentType("text/html;charset=utf-8");
+PrintWriter out = response.getWriter();
+MemberDAO dao = new MemberDAO();
+List<MemberVO>	list = dao.listMembers();
+
+out.print("<html><body>");
+out.print("<table  border=1><tr align='center' bgcolor='lightgreen'>");
+out.print("<td>아이디</td><td>비밀번호</td><td>이름</td><td>이메일</td><td>가입일</td></tr>");
+
+for (int i=0;i<list.size();i++) {
+	MemberVO memberVO = list.get(i);	//새로운 VO를 만들어서 한 튜플씩 저장되어있는 list<Member>로부터 데이터를 읽어옴.
+	String id = memberVO.getId();
+	String pwd = memberVO.getPwd();
+	String name=memberVO.getName();
+	String email=memberVO.getEmail();
+	Date joinDate = memberVO.getJoinDate();
+	out.print("<tr><td>"+id+"</td><td>"+pwd+"</td><td>"+name+"</td><td>"+email+"</td><td>"+joinDate+"</td></tr>");	
+}
+out.print("</table></body></html>");
+*/
